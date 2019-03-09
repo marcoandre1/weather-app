f67941480ae9da99f7ffc5d38c1959ad9d30ddbb
marcoandre1 <31892221+marcoandre1@users.noreply.github.com>
2019-03-08 20:23:57 -0500

    initial commit

---
+++ src/lib/API.js
@@ -0,0 +1,31 @@
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const API_KEY = '422fd74c9da74422855a4e7e2cb2e483';
+const WEATHER_API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/`;
+const GEO_API_URL = `${proxy}https://darksky.net/geo?q=`;
+const ADDRESS_API_URL = `${proxy}https://darksky.net/rgeo?hires=1`;
+
+function getCoordinates(location) {
+  return fetch(`${GEO_API_URL}${location}`)
+    .then(response => response.json());
+}
+
+function getForecast(latitude, longitude) {
+  return fetch(`${WEATHER_API_URL}${latitude},${longitude}`)
+    .then(response => response.json());
+}
+
+function getAddress(latitude, longitude) {
+  return fetch(`${ADDRESS_API_URL}&lat=${latitude}&lon=${longitude}`)
+    .then(response => response.json());
+}
+
+function getEmbedURL(lat, lon) {
+    return `https://maps.darksky.net/@emoji,${lat},${lon},11?domain=&quot;+encodeURIComponent(window.location.href)+&quot;&amp;auth=1552089016_3624c23c92e79156219d8144bcec3006&amp;embed=true&amp;timeControl=false&amp;fieldControl=false&amp;defaultField=emoji`;
+}
+
+export default {
+  getAddress,
+  getForecast,
+  getCoordinates,
+  getEmbedURL
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col-8 offset-2">
+        <div class="input-group mb-3">
+          <input v-model="location" type="text" class="form-control" placeholder="Enter Location">
+          <div class="input-group-append">
+            <button @click="updateLocation" class="btn btn-outline-secondary" type="button">Search</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-8 offset-2 text-center" v-if="forecast">
+        <div class="card text-white bg-secondary mb-3">
+          <div class="card-header">{{address}}</div>
+          <div class="card-body">
+            <h4 class="card-title">{{forecast.currently.summary}}</h4>
+            <div class="card-text icon-temp">
+              <span class="emoji">{{icons[forecast.currently.icon]}}</span>
+              {{forecast.currently.temperature}} °F
+            </div>
+            <div class="card-text">{{forecast.currently.precipProbability}}% chance of precipitation</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-8 offset-2">
+        <iframe
+          id="map-embed-iframe"
+          frameborder="0"
+          height="500px"
+          width="100%"
+          :src="embedULR"
+        ></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import API from "@/lib/API";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  name: "home",
+  data() {
+    return {
+      embedULR: "",
+      location: localStorage.location || "",
+      address: localStorage.address || "",
+      forecast: null,
+      icons: {
+        "clear-day": "😐",
+        "clear-night": "🌝",
+        rain: "☔",
+        snow: "⛄",
+        sleet: "🍦",
+        wind: "💨",
+        fog: "🐸",
+        cloudy: "☁",
+        "partly-cloudy-day": "⛅",
+        "partly-cloudy-night": "🌜"
+      }
+    };
   },
+  mounted() {
+    this.loadWeather(
+      localStorage.lat || "37.8267",
+      localStorage.lon || "-122.4233"
+    );
+  },
+  methods: {
+    loadWeather(lat, lon) {
+      localStorage.lat = lat;
+      localStorage.lon = lon;
+
+      this.embedULR = API.getEmbedURL(lat, lon);
+
+      API.getAddress(lat, lon).then(result => {
+        this.address = [result.name, result.street].join(" ");
+        localStorage.address = this.address;
+      });
+      API.getForecast(lat, lon).then(result => {
+        this.forecast = result;
+      });
+    },
+    updateLocation() {
+      localStorage.location = this.location;
+      API.getCoordinates(this.location).then(result => {
+        this.loadWeather(result.latitude, result.longitude);
+      });
+    }
+  }
 };
 </script>
+
+<style>
+body {
+  font-size: 2em;
+}
+
+.icon-temp {
+  font-size: 2em;
+}
+
+.emoji {
+  font-size: 2em;
+}
+</style>
